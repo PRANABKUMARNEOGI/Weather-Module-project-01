d9a69d2f72000ff543d46c15574ecaef6d33c037
Prankstines <neogipranabkumar1@gmail.com>
2026-06-16 19:09:46 +0530

    Update Weather project module 01.py

--- Weather project module 01.py
+++ Weather project module 01.py
@@ -5,6 +5,7 @@ print(a)
 # integer variable will contain integer values
 integer_value =16237
 print(integer_value, type(integer_value))
+
 # float variable : decimal values
 pi=3.143456788979655
 print(pi, type(pi))
