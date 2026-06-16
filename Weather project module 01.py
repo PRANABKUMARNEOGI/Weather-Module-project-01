0308faf4e31006cd2ce05775879d50d1231316a3
Prankstines <neogipranabkumar1@gmail.com>
2026-06-16 19:06:47 +0530

    Add files via upload

---
+++ Weather project module 01.py
@@ -0,0 +1,17 @@
+# variable : a named memory location
+a=12
+print(a)
+
+# integer variable will contain integer values
+integer_value =16237
+print(integer_value, type(integer_value))
+
+# float variable : decimal values
+pi=3.143456788979655
+print(pi, type(pi))
+
+#boolean variable:
+inthisinetrcecting = True
+print(inthisinetrcecting , type(inthisinetrcecting))
+
+
