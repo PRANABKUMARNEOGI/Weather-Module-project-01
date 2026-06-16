5611e295adb4088cf39ab1b0e4204529c0e57fec
Prankstines <neogipranabkumar1@gmail.com>
2026-06-16 19:09:36 +0530

    Update Weather project module 01.py

--- Weather project module 01.py
+++ Weather project module 01.py
@@ -5,11 +5,9 @@ print(a)
 # integer variable will contain integer values
 integer_value =16237
 print(integer_value, type(integer_value))
-
 # float variable : decimal values
 pi=3.143456788979655
 print(pi, type(pi))
-
 #boolean variable:
 inthisinetrcecting = True
 print(inthisinetrcecting , type(inthisinetrcecting))
